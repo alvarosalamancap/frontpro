71d27e3ec00196492db03fbd976d5f36f66010a0
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-12-12 00:29:56 -0300

    borro error

---
+++ frontPro/src/app/_auth/components/clientmanagement/clientmanagement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clientmanagement',
+  standalone: true,
+  imports: [],
+  templateUrl: './clientmanagement.component.html',
+  styleUrl: './clientmanagement.component.css'
+})
+export class ClientmanagementComponent {
+
+}
