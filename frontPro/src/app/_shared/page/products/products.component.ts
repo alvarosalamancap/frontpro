2b0ea04927f0c36e3325563608e961f4e0690360
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-12-17 11:26:24 -0300

    update test

--- frontPro/src/app/_shared/page/products/products.component.ts
+++ frontPro/src/app/_shared/page/products/products.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { ProductsService } from '../../products.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css'],
+  styleUrl: './products.component.css',
+  providers: [ProductsService],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  productsService = inject(ProductsService);
+  products: any[] = [];
+  filteredProducts: any[] = [];
+  searchQuery: string = '';
+
   constructor(private router: Router) {}
 
-  onLoginClick(): void {
-    // Redirigir a la página de login
-    this.router.navigate(['/login']);
+  ngOnInit(): void {
+    this.productsService
+      .getProducts()
+      .then((data) => {
+        this.products = data.data;
+        this.filteredProducts = this.products;
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  }
+
+  // Función para redirigir al login
+  redirectToLogin(): void {
+    this.router.navigate(['/login']); // Ruta declarada en app.routes.ts
   }
 }
